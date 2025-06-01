d9ab38eef6c417efbef7c2b5b227187ac3223c3c
ethanarndt7 <eba5382@psu.edu>
2025-06-01 19:56:14 -0400

    initial commit - hub

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+        <h1>Hello, Ethan</h1>
+      </header>
+      <main className='main-grid'>
+        <section className='widget'>
+          Clock
+        </section>
+      </main>
+    </div>
+
+
+  );
+}
+
+export default App;
