e930757a410210d8acccd5a35409b858b0056d64
ethanarndt7 <eba5382@psu.edu>
2025-06-01 21:29:17 -0400

    Working clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Clock from './Clock';
+
 
 function App() {
   return (
     <div className="App">
       <div className='greeting animated-greeting'>Hello, Ethan</div>
+<div className="widget">
+  <Clock />
+</div>
+
+
     </div>
 
 
---
+++ src/Clock.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "./Clock.css";
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (date) => {
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+  };
+
+  return (
+    <div className="clock">
+      {formatTime(time)}
+    </div>
+  );
+}
