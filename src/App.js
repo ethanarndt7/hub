aa11ada6c32b05eb0c096764fe6bd4c1fc013748
ethanarndt7 <eba5382@psu.edu>
2025-06-01 20:33:21 -0400

    start of homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className='header'>
-        <h1>Hello, Ethan</h1>
-      </header>
-      <main className='main-grid'>
-        <section className='widget'>
-          Clock
-        </section>
-      </main>
+      <div className='greeting animated-greeting'>Hello, Ethan</div>
     </div>
 
 
