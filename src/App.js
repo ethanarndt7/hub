c44246a324c6751b5aae1f86b00c833b455b08dd
ethanarndt7 <eba5382@psu.edu>
2025-06-03 23:15:18 -0400

    date

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import Clock from './Clock';
 import Weather from './Weather';
 
+const today = new Date().toLocaleDateString(undefined, {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+});
 
 
 function App() {
   return (
     <div className="App">
       <div className='greeting animated-greeting'>Hello, Ethan</div>
+      <div className="date">{today}</div>
 
 <div className="widget">
   <Clock />
